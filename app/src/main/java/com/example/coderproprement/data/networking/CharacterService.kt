a7e4be19b6a91b1b444b7ea573b0b2108064322e
pirpr <quentin.pires@etu.univ-lyon1.fr>
2022-04-15 16:59:45 +0200

    Creation workflow for develop and feature. Creation Action

--- app/src/main/java/com/example/coderproprement/data/networking/CharacterService.kt
+++ app/src/main/java/com/example/coderproprement/data/networking/CharacterService.kt
@@ -3,7 +3,6 @@ package com.example.coderproprement.data.networking
 import fr.quentin.android3.model.Item
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface CharacterService {
     @GET("v1/public/characters?limit=100")
